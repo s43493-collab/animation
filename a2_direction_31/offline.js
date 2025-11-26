42fd50ef3a28154a347f6869af47898f0ab2c2cc
s43493-collab <s43493@bj.ac.th>
2025-11-26 20:49:34 +0700

    Add files via upload

---
+++ a2_direction_31/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1764164829,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/collideobject-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/playeranimation-sheet0.png",
+		"images/playeranimation2-sheet0.png",
+		"images/playeranimation2-sheet1.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
